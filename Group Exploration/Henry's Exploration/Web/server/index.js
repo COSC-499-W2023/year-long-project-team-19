c8504864d4b02f5728152c2c8e4578922a2b987f
Henry Augustiano <henry.augustiano04@gmail.com>
2023-11-09 14:24:15 -0800

    Add login, rules, and cards routes to server

--- Group Exploration/Henry's Exploration/Web/server/index.js
+++ Group Exploration/Henry's Exploration/Web/server/index.js
@@ -17,8 +17,16 @@ mongoose.connect(MONGO_URI, {
   console.log(err);
 });
 
+const loginRouter = require('./routes/loginRoutes');
+const rulesRouter = require('./routes/rulesRoutes');
+const cardsRouter = require('./routes/cardsRoutes');
+
+app.use('/login', loginRouter);
+app.use('/rules', rulesRouter);
+app.use('/cards', cardsRouter);
+
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
 });
 
-
+module.exports = app;
