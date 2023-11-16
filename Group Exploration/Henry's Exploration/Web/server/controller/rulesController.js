c3ec3a8ea5f97c46e80d0fd0c4d923122ef105c3
Henry Augustiano <henry.augustiano04@gmail.com>
2023-11-16 14:38:30 -0800

    Add editRules function to rulesController

--- Group Exploration/Henry's Exploration/Web/server/controller/rulesController.js
+++ Group Exploration/Henry's Exploration/Web/server/controller/rulesController.js
@@ -11,6 +11,30 @@ const showRules = async (req, res) => {
   }
 };
 
+const editRules = async (req, res) => {
+ try {
+  const { order = null, context = null, title = null, _id } = req.body; // order, title and context are optional
+  const rules = await Rules.findOne({ _id });
+
+  if (!rules) {
+    return res.status(404).json({ message: 'Rule not found'});
+  }
+
+  rules.order = order !== null ? order : rules.order;
+  rules.context = context !== null ? context : rules.context;
+  rules.title = title !== null ? title : rules.title;
+  
+  rules.markModified('rules');
+  rules.save();
+
+  return res.status(200).json({ message: 'Rule updated', rules: rules });
+
+ } catch (error) {
+  return res.status(500).json({ message: 'Server error'});
+ }
+};
+
 module.exports = {
   showRules,
+  editRules,
 };
