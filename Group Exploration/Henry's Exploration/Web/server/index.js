58cdbcc29e90e704071bf92aa44523962a409f6b
Henry Augustiano <henry.augustiano04@gmail.com>
2023-11-16 15:46:37 -0800

    Add mock commit to test Vercel

--- Group Exploration/Henry's Exploration/Web/server/index.js
+++ Group Exploration/Henry's Exploration/Web/server/index.js
@@ -24,6 +24,7 @@ const cardsRouter = require('./routes/cardsRoutes');
 app.use('/api/user', userRouter);
 app.use('/api/rules', rulesRouter);
 app.use('/api/cards', cardsRouter);
+// mock commit to test vercel
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
