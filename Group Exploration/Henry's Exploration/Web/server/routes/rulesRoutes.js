799d692b9bce7b5bab9ca95a8fb32b7254c9a7ba
Henry Augustiano <henry.augustiano04@gmail.com>
2023-11-16 14:38:34 -0800

    Add route for editing rules

--- Group Exploration/Henry's Exploration/Web/server/routes/rulesRoutes.js
+++ Group Exploration/Henry's Exploration/Web/server/routes/rulesRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const rulesController = require('../controller/rulesController');
 
 router.get('/showRules', rulesController.showRules);
+router.post('/editRules', rulesController.editRules)
 
 module.exports = router;
