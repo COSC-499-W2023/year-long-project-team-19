4c21656e1260910e41a7c55118a5c38302b319c2
Henry Augustiano <henry.augustiano04@gmail.com>
2023-11-22 10:10:06 -0800

    Delete unnecessary files and update dependencies

---
+++ Group Exploration/Henry's Exploration/Web/client/src/components/Home.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div style={{ backgroundColor: 'red', height: '100vh' }}>
+      <h1>Welcome to the Homepage</h1>
+    </div>
+  );
+};
+
+export default Home;
