f26bac29a8a31195cb333bd404cb3729f900b8c5
Henry Augustiano <henry.augustiano04@gmail.com>
2023-11-22 10:51:59 -0800

    move inline css to index.css

--- Group Exploration/Henry's Exploration/Web/client/src/components/Home.jsx
+++ Group Exploration/Henry's Exploration/Web/client/src/components/Home.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import Navbar from './Navbar';
 const Home = () => {
   return (
-    <div style={{ backgroundColor: 'blanchedalmond', height: '100vh' }}>
+    <>
       <Navbar />
       <div className="game-container">
         <iframe src="https://i.simmer.io/@dk2706/democcg-v1" style={{ width: '960px', height: '600px' }}></iframe>
       </div>
-    </div>
+    </>
   );
 };
 
