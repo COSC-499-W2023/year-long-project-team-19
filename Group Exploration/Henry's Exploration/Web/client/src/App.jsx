4c8b14816c997cb26ca24949da5d48c376d6312f
Henry Augustiano <henry.augustiano04@gmail.com>
2023-11-22 10:52:58 -0800

    Add Rules component to Route

--- Group Exploration/Henry's Exploration/Web/client/src/App.jsx
+++ Group Exploration/Henry's Exploration/Web/client/src/App.jsx
@@ -1,11 +1,12 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
-
+import Rules from './components/Rules';
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/rules" element={<Rules />} />
       </Routes>
     </Router>
   );
