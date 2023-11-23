add77f40db4a40803c2e0adde3cbadcc284ddb8f
Henry Augustiano <henry.augustiano04@gmail.com>
2023-11-23 13:54:49 -0800

    Add Cards to routing

--- Group Exploration/Henry's Exploration/Web/client/src/App.jsx
+++ Group Exploration/Henry's Exploration/Web/client/src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import Rules from './components/Rules';
+import Cards from './components/Cards';
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/rules" element={<Rules />} />
+        <Route path="/cards" element={<Cards />} />
       </Routes>
     </Router>
   );
