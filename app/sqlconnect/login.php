42e3191531974ea9c01d03ed35b8a284f48a1826
Brenner <43012551+Prelude14@users.noreply.github.com>
2023-11-28 02:34:59 -0800

    Update login.php
    added some basic sql injection checks before the sql query is run using the inputted username. Didn't add any for the inputted password because it is never entered into a query anyways, so it shouldn't be able to cause this tho.

--- app/sqlconnect/login.php
+++ app/sqlconnect/login.php
@@ -11,16 +11,33 @@
 	}
 
 	//grab unity's values that it sent, and store in local variable to be queried in our database
-	$username = $_POST["username"];
+	$username = mysqli_real_escape_string($con, $_POST["username"]); //real escape string is built in sql injection checker, will strip any sql commands out
+	//going to create a second username variable to further filter the user's inputted username to avoid sql injection
+	$usernameClean = filter_var($username, FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_LOW | FILTER_FLAG_STRIP_HIGH);
+	//filter flag strip will take out any characters from the username that aren't just the standard ASCII text characters (values 0-32 is special--the low strip takes these out, and any above 128 is 
+	//special as well, so the strip high takes care of these)
+	
+	//CHECK IF USERNAME CLEAN MATCHES USERNAME TO RETURN ERROR CODE
+	if ($username != $usernameClean) {
+		echo "2: The inputted username contains some prohibitted characters, possibly some sql code.";//error 2, the usernameClean doesn't match the entered username, 
+		//so we exit before the sql query, as to avoid sql injection
+		exit();
+	}
+	//CKECK IF USERNAME CLEAN IS A VALID EMAIL
+	if (!filter_var($usernameClean, FILTER_VALIDATE_EMAIL)) { //The easiest and safest way to check whether an email address is well-formed 
+  		echo "3: Invalid email format, the inputted username is not a valid email address."; //error 3, username clean isn't a valid email address, could be missing the @, or maybe a .com etc.
+	}
+
+	//don't technically need to check against sql inject on the password, since it is never used in a query, just in a crypt function to be compare to the user's hash.
 	$password = $_POST["password"];
 
 
 	//Query to check database for that USERNAME, then its password and other info ==============================================
-	$namequery = "SELECT useremail, hash, salt, datecreated, gamesplayed, gameswon, damagedealt FROM useracc WHERE useremail ='" .$username. "';";
+	$namequery = "SELECT useremail, hash, salt, datecreated, gamesplayed, gameswon, damagedealt FROM useracc WHERE useremail ='" .$usernameClean. "';";
     //win loss ratio might need to be caluclated (not stored in table?)
 
 	//Run the Query 
-	$usernameCheck = mysqli_query($con, $namequery) or die("2: Name Check failed."); //error 2 for name check query failed
+	$usernameCheck = mysqli_query($con, $namequery) or die("4: Name Check failed."); //error 4 for name check query failed
 
 	//if there is NO account matching that username, OR there is MORE THAN 1, then something is wrong and we want to quit
 	if (mysqli_num_rows($usernameCheck) != 1) //specifically counts ROWS
