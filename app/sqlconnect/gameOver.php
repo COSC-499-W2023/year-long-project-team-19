cb71bee7fd2e211e6d7ad6bf04fc6b0154c0c20b
lhenon999 <77656081+lhenon999@users.noreply.github.com>
2024-04-03 13:31:53 -0700

    Added gameOver.php to update player stats on game end

---
+++ app/sqlconnect/gameOver.php
@@ -0,0 +1,25 @@
+<?php 
+
+	// error_reporting(E_ALL);
+	// ini_set('display_errors', 1);
+
+	$con = mysqli_connect("localhost", "root2", "Cosc499Team19DuelTimejkwndjfifi", "cosc499");
+
+	if (mysqli_connect_errno()) 
+	{
+		echo "1:Connection failed."; 
+		exit();
+	}
+
+	$username = mysqli_real_escape_string($con, $_POST["username"]);
+	$result = $_POST["result"];
+	
+	if ($result == 'w'){
+		$updateStats = "UPDATE useracc SET gamesplayed = gamesplayed + 1, gameswon = gameswon + 1 WHERE useremail = '$username'";
+	} else if ($result == 'l'){
+		$updateStats = "UPDATE useracc SET gamesplayed = gamesplayed + 1 WHERE useremail = '$username'";
+	}
+	
+	$updateStats = mysqli_query($con, $updateStats) or die("2: Update failed."); 
+	
+	echo "0";
