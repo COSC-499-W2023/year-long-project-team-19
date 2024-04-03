59a64c310ea458f443cea0639542e4c38e778e1c
lhenon999 <77656081+lhenon999@users.noreply.github.com>
2024-04-03 16:15:34 -0700

    updated gameOver.php for damageDealt

--- app/sqlconnect/gameOver.php
+++ app/sqlconnect/gameOver.php
@@ -13,11 +13,12 @@
 
 	$username = mysqli_real_escape_string($con, $_POST["username"]);
 	$result = $_POST["result"];
+	$damage = $_POST["damageDealt"];
 	
 	if ($result == 'w'){
-		$updateStats = "UPDATE useracc SET gamesplayed = gamesplayed + 1, gameswon = gameswon + 1 WHERE useremail = '$username'";
+		$updateStats = "UPDATE useracc SET gamesplayed = gamesplayed + 1, gameswon = gameswon + 1, damagedealt = damagedealt + $damage WHERE useremail = '$username'";
 	} else if ($result == 'l'){
-		$updateStats = "UPDATE useracc SET gamesplayed = gamesplayed + 1 WHERE useremail = '$username'";
+		$updateStats = "UPDATE useracc SET gamesplayed = gamesplayed + 1, damagedealt = damagedealt + $damage WHERE useremail = '$username'";
 	}
 	
 	$updateStats = mysqli_query($con, $updateStats) or die("2: Update failed."); 
